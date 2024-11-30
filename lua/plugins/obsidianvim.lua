39430be218c6ee25470c804a4e17d0e09e8341f1
sigma2647 <1169446204@qq.com>
2024-11-30 18:14:40 +0800

    d

--- lua/plugins/obsidianvim.lua
+++ lua/plugins/obsidianvim.lua
@@ -1,47 +1,63 @@
 return {
-  --  "epwalsh/obsidian.nvim",
-  --  version = "*", -- recommended, use latest release instead of latest commit
-  --  lazy = true,
-  --  ft = "markdown",
-  --  keys = {
-  --    { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note", mode = "n" },
-  --    { "<leader>oo", "<cmd>ObsidianSearch<cr>", desc = "Search Obsidian notes", mode = "n" },
-  --    { "<leader>os", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switch", mode = "n" },
-  --    { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show location list of backlinks", mode = "n" },
-  --    { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link under cursor", mode = "n" },
-  --    { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "n" },
-  --    { "<C-l>", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "i" },
-  --    { "<C-]>", "<cmd>ObsidianTomorrow<cr>", desc = "Follow link under cursor", mode = "n" },
-  --    { "<C-[>", "<cmd>ObsidianYesterday<cr>", desc = "Follow link under cursor", mode = "n" },
-  --  },
-  --  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  --  -- event = {
-  --  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --  --   "BufReadPre path/to/my-vault/**.md",
-  --  --   "BufNewFile path/to/my-vault/**.md",
-  --  -- },
-  --  dependencies = {
-  --    -- Required.
-  --    "nvim-lua/plenary.nvim",
-  --
-  --    -- see below for full list of optional dependencies 👇
-  --  },
-  --  opts = {
-  --    workspaces = {
-  --      {
-  --        name = "",
-  --        path = "~/note/sigma",
-  --      },
-  --    },
-  --
-  --    -- see below for full list of options 👇
-  --    templates = {
-  --      subdir = "template",
-  --    },
-  --    daily_notes = {
-  --      -- Optional, if you keep daily notes in a separate directory.
-  --      folder = "pages/diary",
-  --    },
-  --  },
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    ft = "markdown",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "sigma",
+                path = "~/note/sigma",
+            },
+        },
+    }
+
+    --  lazy = true,
+    --  "epwalsh/obsidian.nvim",
+    --  version = "*", -- recommended, use latest release instead of latest commit
+    --  lazy = true,
+    --  ft = "markdown",
+    --  keys = {
+    --    { "<leader>on", "<cmd>ObsidianNew<cr>", desc = "New Obsidian note", mode = "n" },
+    --    { "<leader>oo", "<cmd>ObsidianSearch<cr>", desc = "Search Obsidian notes", mode = "n" },
+    --    { "<leader>os", "<cmd>ObsidianQuickSwitch<cr>", desc = "Quick Switch", mode = "n" },
+    --    { "<leader>ob", "<cmd>ObsidianBacklinks<cr>", desc = "Show location list of backlinks", mode = "n" },
+    --    { "<leader>of", "<cmd>ObsidianFollowLink<cr>", desc = "Follow link under cursor", mode = "n" },
+    --    { "<leader>ot", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "n" },
+    --    { "<C-l>", "<cmd>ObsidianTemplate<cr>", desc = "Follow link under cursor", mode = "i" },
+    --    { "<C-]>", "<cmd>ObsidianTomorrow<cr>", desc = "Follow link under cursor", mode = "n" },
+    --    { "<C-[>", "<cmd>ObsidianYesterday<cr>", desc = "Follow link under cursor", mode = "n" },
+    --  },
+    --  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    --  -- event = {
+    --  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+    --  --   "BufReadPre path/to/my-vault/**.md",
+    --  --   "BufNewFile path/to/my-vault/**.md",
+    --  -- },
+    --  dependencies = {
+    --    -- Required.
+    --    "nvim-lua/plenary.nvim",
+    --
+    --    -- see below for full list of optional dependencies 👇
+    --  },
+    --  opts = {
+    --    workspaces = {
+    --      {
+    --        name = "",
+    --        path = "~/note/sigma",
+    --      },
+    --    },
+    --
+    --    -- see below for full list of options 👇
+    --    templates = {
+    --      subdir = "template",
+    --    },
+    --    daily_notes = {
+    --      -- Optional, if you keep daily notes in a separate directory.
+    --      folder = "pages/diary",
+    --    },
+    --  },
 }
